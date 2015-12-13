a9e440e7f5542a9bf210b4e3efe2dac3ae1f214b
sidestepism <fushimi.ryohe.i@gmail.com>
2015-12-13 10:35:14 +0900

    first commit

---
+++ pccb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pccb
+//
+//  Created by Ryohei Fushimi on 2015/12/13.
+//  Copyright © 2015 fushimir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
