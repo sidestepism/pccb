266d35b8ed393d9cf1373b2ab16b0ba9139be954
sidestepism <fushimi.ryohe.i@gmail.com>
2015-12-13 10:43:19 +0900

    1-6

--- pccb/main.cpp
+++ pccb/main.cpp
@@ -5,11 +5,33 @@
 //  Created by Ryohei Fushimi on 2015/12/13.
 //  Copyright © 2015 fushimir. All rights reserved.
 //
-
-#include <iostream>
+#include "pccb.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n = 5;
+    int a[] = {2, 3, 4, 5, 6};
+    int ans = 0;
+
+    int i, j, k;
+    REP(i, n){
+        REP(j, n){
+            REP(k, n){
+                if(a[i] + a[j] <= a[k]){
+                    continue;
+                }
+                if(a[j] + a[k] <= a[i]){
+                    continue;
+                }
+                if(a[k] + a[i] <= a[j]){
+                    continue;
+                }
+//                dump(ans);
+                ans = max(ans, a[i] + a[j] + a[k]);
+            }
+        }
+    }
+    printf("%d", ans);
 }
