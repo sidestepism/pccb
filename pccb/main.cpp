580269de152dabbc281648eedf542858024aebc1
sidestepism <fushimi.ryohe.i@gmail.com>
2015-12-13 11:19:50 +0900

    1-1 kujibiki O(n^2logn)

--- pccb/main.cpp
+++ pccb/main.cpp
@@ -14,18 +14,29 @@ int main(int argc, const char * argv[]) {
     int target = 10;
     vector<int>k = {1, 5, 11, 2, 9, 12};
     int ans = false;
+
+    vector<int> kk;
+    REP(c, n){
+        REP(d, n){
+            kk.push_back(k[c] + k[d]);
+        }
+    }
+    sort(kk.begin(), kk.end());
+    
     REP(a, n){
         REP(b, n){
-            REP(c, n){
-//                REP(d, n){
-//                    if(k[a] + k[b] + k[c] + k[d] == target){
-//                        ans = true;
-//                    }
-//                }
-                // find d s.t. k_d = m - k_a - k_b - k_c
-                int dt = n - k[a] - k[b] - k[c];
-                ans |= binary_search(k.begin(), k.end(), dt);
-            }
+            int dt = n - k[a] - k[b];
+            ans |= binary_search(k.begin(), k.end(), dt);
+//            REP(c, n){
+////                REP(d, n){
+////                    if(k[a] + k[b] + k[c] + k[d] == target){
+////                        ans = true;
+////                    }
+////                }
+//                // find d s.t. k_d = m - k_a - k_b - k_c
+//                int dt = n - k[a] - k[b] - k[c];
+//                ans |= binary_search(k.begin(), k.end(), dt);
+//            }
         }
     }
     dump(ans);
